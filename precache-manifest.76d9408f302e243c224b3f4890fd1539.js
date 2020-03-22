e67bfa4c5f216e157c4f099c99e7eed581e36ddc
izekki.mohamed@gmail <izekki050619933>
2020-03-22 21:41:48 +0100

    commit

---
+++ precache-manifest.76d9408f302e243c224b3f4890fd1539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fa67732714935046e8d761c66e86bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed9374ffa684ed19f50",
+    "url": "/static/css/main.131e79ed.chunk.css"
+  },
+  {
+    "revision": "2e51b773547ab13b648d",
+    "url": "/static/js/2.b6699ce0.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.b6699ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed9374ffa684ed19f50",
+    "url": "/static/js/main.b8131eae.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
